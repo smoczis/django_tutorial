6597e1ab46563c0990c75530008be73d3f47106e
Romanowski Mateusz <rb26049G@raiffeisen.pl>
2019-03-06 12:49:39 +0100

    Add path to urls

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5/
+    # the 'name' value as called by the {% url %} template tag
     path('<int:question_id>/', views.detail, name='detail'),
+    # similar to above
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
