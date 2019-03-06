41d74151b304f7ecc7c03d5807968892d74eea97
Romanowski Mateusz <rb26049G@raiffeisen.pl>
2019-03-06 12:10:57 +0100

    Provide shortcut for rendering template

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 
 from .models import Question
 
-
+"""
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template("polls/index.html")
@@ -11,6 +12,11 @@ def index(request):
         'latest_questions': latest_questions
     }
     return HttpResponse(template.render(context, request))
+"""
+def index(request):
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_questions': latest_questions}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
