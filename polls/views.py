142a777b87bd7bed1761f95822678700c66e20b0
Romanowski Mateusz <rb26049G@raiffeisen.pl>
2019-03-06 12:08:12 +0100

    Add simple index view using template

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,25 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
+from .models import Question
+
+
 def index(request):
-    return HttpResponse("Hello world!")
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        'latest_questions': latest_questions
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You are looking at question {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You are looking at the result of question {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting for question {question_id}")
