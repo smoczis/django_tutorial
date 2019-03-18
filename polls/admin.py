b46a990c4b0d5c8840c5a71c8824e28a039ca420
Romanowski Mateusz <rb26049G@raiffeisen.pl>
2019-03-18 14:22:35 +0100

    Link Choices with corresponding Questions while adding new Question

--- polls/admin.py
+++ polls/admin.py
@@ -2,12 +2,17 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Basic fields', {'fields': ['question_text']}),
-        ("Date information", {'fields': ['pub_date']})
+        ("Date information", {'fields': ['pub_date'], 'classes': ['collapse']})
     ]
+    inlines = [ChoiceInLine]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
